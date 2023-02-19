1ebe4e2b3b755c812984ab474a45a4c4eb1d9140
Michal Grzegorczyk <michalgrzegorczyk@michals-macbook-air.home>
2023-02-20 00:25:20 +0100

    XD

--- apps/web/src/app/App.jsx
+++ apps/web/src/app/App.jsx
@@ -19,3 +19,4 @@ export function App() {
 }
 
 export default App;
+
--- apps/web/src/app/components/Modal.jsx
+++ apps/web/src/app/components/Modal.jsx
@@ -11,6 +11,7 @@ const Modal = () => {
           <h4>{title}</h4>
           <div dangerouslySetInnerHTML={ {__html: answer} }></div>
           <button className="btn btn-hipster close-btn" onClick={closeModal}>Zamknij</button>
+          <button className="btn btn-hipster">Edytuj</button>
         </div>
       </div>
     </aside>
